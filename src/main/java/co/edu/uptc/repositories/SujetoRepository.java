8578d799b1a1b55529c84f3646fcffdaf9040248
HarDep <haroldralvaradol@gmail.com>
2023-11-09 23:11:59 -0500

    Update find sellers query

--- src/main/java/co/edu/uptc/repositories/SujetoRepository.java
+++ src/main/java/co/edu/uptc/repositories/SujetoRepository.java
@@ -14,10 +14,11 @@ public interface SujetoRepository extends JpaRepository<Sujeto, Integer> {
     //query para obtener empleados que son vendedores y que atienden en la hora y día especificado
     @Query(value = "SELECT * FROM sujetos WHERE id_sujeto IN (SELECT id_empleado FROM empleados_roles " +
             "WHERE id_empleado IN (SELECT id_empleado FROM empleados_horarios WHERE id_horario IN (" +
-            "SELECT id_horario FROM horarios WHERE dia_semana_horario = :dia AND " +
-            "hora_inicio_horario <= CONVERT(TIME, :hora) AND DATEADD(HOUR, numero_horas_horario, " +
-            "hora_inicio_horario) >= CONVERT(TIME, :hora))) AND id_rol_empleado IN (" +
-            "SELECT id_rol_empleado FROM roles_empleado WHERE nombre_rol_empleado LIKE 'vendedor'))", nativeQuery = true)
+            "SELECT id_horario FROM horarios WHERE dia_semana_horario = :dia AND hora_inicio_horario " +
+            "<= CONVERT(TIME, :hora) AND DATEADD(HOUR, numero_horas_horario, hora_inicio_horario) >= " +
+            "CONVERT(TIME, :hora)) AND es_activo_empleado_horario = 1) AND id_rol_empleado IN (SELECT " +
+            "id_rol_empleado FROM roles_empleado WHERE nombre_rol_empleado LIKE 'vendedor') AND " +
+            "es_activo_empleado_rol = 1)", nativeQuery = true)
     List<Sujeto> findSellers(@Param("hora") String hora,@Param("dia") String dia);
 
     //query para obtener proveedores
